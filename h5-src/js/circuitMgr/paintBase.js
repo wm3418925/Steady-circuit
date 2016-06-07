7a2e479aa0f4e21703e3322e4d0e069f1dbc4d13
wm3418925 <591593903@qq.com>
2016-06-07 21:02:19 +0800

    导线宽度改为2, 修改控件图

--- h5-src/js/circuitMgr/paintBase.js
+++ h5-src/js/circuitMgr/paintBase.js
@@ -35,7 +35,7 @@ Manager.PaintCrunText = function(c) {
 Manager.PaintLead = function(l) {
 	ASSERT(l != null);
 	Manager.ctx.strokeStyle = PaintCommonFunc.HexToRGBStr(l.color);
-	Manager.ctx.lineWidth = 1;
+	Manager.ctx.lineWidth = 2;
 	Manager.ctx.beginPath();
 	l.PaintLead(Manager.ctx);
 	Manager.ctx.stroke();
