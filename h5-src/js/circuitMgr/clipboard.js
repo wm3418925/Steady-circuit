4dc56baa0badec32817da9300dbe8d0caf1833ac
wm3418925 <591593903@qq.com>
2016-06-18 23:34:38 +0800

    editMess.js clipboard.js

--- h5-src/js/circuitMgr/clipboard.js
+++ h5-src/js/circuitMgr/clipboard.js
@@ -1,98 +1,77 @@
 
-//10处理剪切板函数-----------------------------------------------------------------↓
-void Manager.ClearClipboard()
 //清空剪切板
-{
-	if (Manager.clipBody.IsOnCrun())
-		delete Manager.clipBody.p2;
-	else if (Manager.clipBody.IsOnCtrl())
-		delete Manager.clipBody.p3;
+Manager.ClearClipboard = function() {
 	Manager.clipBody.Clear();
-}
+};
 
-bool Manager.GetClipboardState()
 //获取剪切板是否可用
-{
+Manager.GetClipboardState = function() {
 	return Manager.clipBody.IsOnBody();
-}
+};
 
-void Manager.CopyToClipboard(const Pointer &body)
 //拷贝body指向的物体到剪切板
-{
+Manager.CopyToClipboard = function(body) {
 	ASSERT(body.IsOnBody());
 	Manager.motiCount = 0;
-	ClearClipboard();	//清空剪切板
+	Manager.ClearClipboard();	//清空剪切板
 
 	if (body.IsOnCrun())
-		Manager.clipBody.SetOnCrun(body.p2.Clone(CLONE_FOR_CLIPBOARD), true);
+		Manager.clipBody.SetOnCrun(body.p.Clone(CLONE_FOR_CLIPBOARD), true);
 	else //if (body.IsOnCtrl())
-		Manager.clipBody.SetOnCtrl(body.p3.Clone(CLONE_FOR_CLIPBOARD), true);
-}
+		Manager.clipBody.SetOnCtrl(body.p.Clone(CLONE_FOR_CLIPBOARD), true);
+};
 
-Pointer Manager.CopyBody(FOCUS_OR_POS &body)
 //复制物体到剪切板
-{
-	Pointer pointer = Manager.GetBodyPointer(body);
+Manager.CopyBody = function(body) {
+	var pointer = Manager.GetBodyPointer(body);
 	if (!pointer.IsOnBody()) return pointer;
-	CopyToClipboard(pointer);
+	Manager.CopyToClipboard(pointer);
 	return pointer;
-}
+};
 
-void Manager.CutBody(FOCUS_OR_POS &body)
 //剪切物体到剪切板
-{
-	Pointer pointer = CopyBody(body);	//复制物体
+Manager.CutBody = function(body) {
+	var pointer = CopyBody(body);	//复制物体
 	if (!pointer.IsOnBody()) return;
-	Delete(pointer);					//删除物体
-	PaintAll();							//重绘电路
-}
+	Manager.Delete(pointer);		//删除物体
+	Manager.PaintAll();				//重绘电路
+};
 
-bool Manager.PasteBody(POINT pos)
 //粘贴物体
-{
-	if (!Manager.clipBody.IsOnBody())
-	{
+Manager.PasteBody = function(pos) {
+	if (!Manager.clipBody.IsOnBody()) {
 		MessageBeep(0);
 		return false;
 	}
 	DPtoLP(pos, Manager.canvas);
 
-	if (Manager.clipBody.IsOnCrun())
-	{
-		if (Manager.crun.length >= MAX_CRUN_COUNT)
-		{
+	if (Manager.clipBody.IsOnCrun()) {
+		if (Manager.crun.length >= MAX_CRUN_COUNT) {
 			Manager.canvas.MessageBox("结点超过最大数量!", "结点不能添加", MB_ICONWARNING);
 			return false;
 		}
 
-		CloneCircuitBeforeChange();	//编辑前复制电路
 		//编辑部分
-		crun[Manager.crun.length] = Manager.clipBody.p2.Clone(CLONE_FOR_USE);
-		crun[Manager.crun.length].coord = pos;
-		crun[Manager.crun.length].num = Manager.crun.length;
-		++ Manager.crun.length;
+		var newElement = Manager.clipBody.p2.Clone(CLONE_FOR_USE);
+		newElement.x = pos.x; newElement.y = pos.y;
+		newElement.index = Manager.crun.length;
+		Manager.crun.push(newElement);
 
-		PutCircuitToVector();	//将新的电路信息保存到容器
-		PaintCrun(crun[Manager.crun.length-1]);
-	}
-	else if (Manager.clipBody.IsOnCtrl())
-	{
-		if (Manager.ctrl.length >= MAX_CTRL_COUNT)
-		{
+		Manager.PaintCrun(newElement);
+	} else if (Manager.clipBody.IsOnCtrl()) {
+		if (Manager.ctrl.length >= MAX_CTRL_COUNT) {
 			Manager.canvas.MessageBox("电学元件超过最大数量!", "电学元件不能添加", MB_ICONWARNING);
 			return false;
 		}
 
-		CloneCircuitBeforeChange();	//编辑前复制电路
 		//编辑部分
-		ctrl[Manager.ctrl.length] = Manager.clipBody.p3.Clone(CLONE_FOR_USE);
-		ctrl[Manager.ctrl.length].coord = pos;
-		ctrl[Manager.ctrl.length].num = Manager.ctrl.length;
-		++ Manager.ctrl.length;
+		var newElement = Manager.clipBody.p.Clone(CLONE_FOR_USE);
+		newElement.x = pos.x; newElement.y = pos.y;
+		newElement.index = Manager.ctrl.length;
+		Manager.ctrl.push(newElement);
 
-		PutCircuitToVector();	//将新的电路信息保存到容器
-		PaintCtrl(ctrl[Manager.ctrl.length-1]);
+		Manager.PaintCtrl(newElement);
 	}
 
 	return true;
-}
+};
--- h5-src/js/circuitMgr/editMess.js
+++ h5-src/js/circuitMgr/editMess.js
@@ -1,110 +1,93 @@
 
-bool Manager.AddBody(POINT pos)
 //添加物体
-{
-	BODY_TYPE temp = addState;
+Manager.AddBody = function(pos) {
+	var temp = Manager.addState;
 
-	addState = BODY_NO;	//不再添加物体
+	Manager.addState = BODY_NO;	//不再添加物体
 	DPtoLP(pos, Manager.canvas);
 
-	if (BODY_CRUN == temp)
-	{
-		if (Manager.crun.length >= MAX_CRUN_COUNT)
-		{
+	if (BODY_CRUN == temp) {
+		if (Manager.crun.length >= MAX_CRUN_COUNT) {
 			Manager.canvas.MessageBox("结点超过最大数量!", "结点不能添加", MB_ICONWARNING);
 			return false;
 		}
 
-		CloneCircuitBeforeChange();	//编辑前复制电路
-		AddCrun(pos);				//编辑函数
-		PutCircuitToVector();		//将新的电路信息保存到容器
+		Manager.AddCrun(pos);				//编辑函数
 		return true;
-	}
-	else if (Pointer.IsCtrl(temp))
-	{
-		if (Manager.ctrl.length >= MAX_CTRL_COUNT)
-		{
+	} else if (Pointer.IsCtrl(temp)) {
+		if (Manager.ctrl.length >= MAX_CTRL_COUNT) {
 			Manager.canvas.MessageBox("电学元件超过最大数量!", "电学元件不能添加", MB_ICONWARNING);
 			return false;
 		}
 
-		CloneCircuitBeforeChange();	//编辑前复制电路
-		AddCtrl(pos, temp);			//编辑函数
-		PutCircuitToVector();		//将新的电路信息保存到容器
+		Manager.AddCtrl(pos, temp);			//编辑函数
 		return true;
-	}
-	else
-	{
+	} else {
 		return false;
 	}
-}
+};
 
-void Manager.Property(FOCUS_OR_POS &body, bool isReadOnly)
 //显示和改变物体属性
-{
-	char tempStr[NAME_LEN*3];
-	LISTDATA list;
-	CDC * model = null;
-	Pointer pointer = Manager.GetBodyPointer(body);
+Manager.Property = function(body, isReadOnly) {
+	var tempStr;
+	var list = LISTDATA.CreateNew();
+	var model = null;
+	var pointer = Manager.GetBodyPointer(body);
 
 	if (pointer.IsOnLead()) {
 		tempStr = Manager.GetBodyDefaultName(pointer) + " 的颜色";	//窗口标题
-		pointer.p1.GetDataList(tempStr, &list);	//数据
+		pointer.p.GetDataList(tempStr, list);	//数据
 	} else if (pointer.IsOnCrun()) {
 		tempStr = Manager.GetBodyDefaultName(pointer)  + " 的标签";	//窗口标题
-		pointer.p2.GetDataList(&list);				//数据
-		model = &crunImageData;						//示例
+		pointer.p.GetDataList(list);				//数据
+		model = crunImageData;						//示例
 	} else if (pointer.IsOnCtrl()) {
 		tempStr = Manager.GetBodyDefaultName(pointer) + " 的标签和电学属性";	//窗口标题
-		pointer.p.GetDataList(&list);				//数据
+		pointer.p.GetDataList(list);				//数据
 		model = Manager.GetCtrlPaintImage(pointer.p);		//示例
 	} else {
 		return;
 	}
 
 	Manager.PaintWithSpecialColorAndRect(pointer, false);
-	MyPropertyDlg dlg(&list, isReadOnly, model, tempStr, Manager.canvas);
+	var dlg = MyPropertyDlg.CreateNew(list, isReadOnly, model, tempStr, Manager.canvas);
 	dlg.DoModal();
-}
+};
 
-void Manager.ChangeCtrlStyle(FOCUS_OR_POS &body)
 //改变电学元件类型
-{
-	BODY_TYPE preStyle, newStyle;
-	char tempStr[NAME_LEN*3];
+Manager.ChangeCtrlStyle = function(body) {
+	var preStyle, newStyle;
+	var tempStr;
 
-	Pointer pointer = Manager.GetBodyPointer(body);
+	var pointer = Manager.GetBodyPointer(body);
 	if (!pointer.IsOnCtrl()) return;
 
 	//获得原来类型
-	preStyle = newStyle = pointer.p3.style;
+	preStyle = newStyle = pointer.p.style;
 
 	//初始化list数据
-	LISTDATA list;
-	list.Init(1);
-	list.SetAEnumMember("电学元件的类型", &newStyle, ENUM_CTRL);
+	var list = LISTDATA.CreateNew();
+	list.SetAEnumMember("电学元件的类型", newStyle, ENUM_CTRL);
 
 	//获得窗口标题
 	tempStr = Manager.GetBodyDefaultName(pointer) + " 的类型";
 
 	//显示对话框
 	Manager.PaintWithSpecialColorAndRect(pointer, false);
-	MyPropertyDlg dlg(&list, false, Manager.GetCtrlPaintImage(pointer.p3), tempStr, Manager.canvas);
+	var dlg = MyPropertyDlg.CreateNew(list, false, Manager.GetCtrlPaintImage(pointer.p), tempStr, Manager.canvas);
 	dlg.DoModal();
 
 	//改变类型
-	if (preStyle != newStyle)
-	{
+	if (preStyle != newStyle) {
 		if (IDYES != alert("改变类型会丢失原有电学元件的数据!\n继续吗?", MB_YESNO)) return;
-		pointer.p3.ChangeStyle(newStyle);
+		pointer.p.ChangeStyle(newStyle);
 	}
-}
+};
 
-void Manager.PosBodyMove(Pointer * body, POINT firstPos, POINT lastPos)
 //移动物体
-{
-	int i;
-	POINT inter;
+Manager.PosBodyMove = function(body, firstPos, lastPos) {
+	var i;
+	var inter = {};
 
 	//获得相对坐标
 	inter.x = lastPos.x - firstPos.x;
@@ -112,84 +95,63 @@ void Manager.PosBodyMove(Pointer * body, POINT firstPos, POINT lastPos)
 	if (inter.x==0 && inter.y==0) return;
 
 	ASSERT(body.IsOnBody());
-	if (body.IsOnCrun())
-	{
+	if (body.IsOnCrun()) {
 		body.p.x += inter.x;
 		body.p.y += inter.y;
-		for (i=0; i<4; ++i) if (body.p2.lead[i])
-			body.p2.lead[i].RefreshPos();
-	}
-	else //if (body.IsOnCtrl())
-	{
+		for (i=0; i<4; ++i) if (body.p.lead[i])
+			body.p.lead[i].RefreshPos();
+	} else { //if (body.IsOnCtrl())
 		body.p.x += inter.x;
 		body.p.y += inter.y;
-		for (i=0; i<2; ++i) if (body.p3.lead[i])
-			body.p3.lead[i].RefreshPos();
+		for (i=0; i<2; ++i) if (body.p.lead[i])
+			body.p.lead[i].RefreshPos();
 	}
-}
+};
 
-bool Manager.PosBodyClone(const Pointer * body, POINT firstPos, POINT lastPos)
 //在指定位置复制物体
-{
+Manager.PosBodyClone = function(body, firstPos, lastPos) {
 	//获得相对坐标
-	POINT inter;
+	var inter = {};
 	inter.x = lastPos.x - firstPos.x;
 	inter.y = lastPos.y - firstPos.y;
 
 	//复制
-	if (body.IsOnCrun())
-	{
+	if (body.IsOnCrun()) {
 		//验证
-		if (Manager.crun.length >= MAX_CRUN_COUNT)
-		{
+		if (Manager.crun.length >= MAX_CRUN_COUNT) {
 			Manager.canvas.MessageBox("结点超过最大数量!", "结点不能添加", MB_ICONWARNING);
 			return false;
 		}
 
-		//编辑前复制电路
-		CloneCircuitBeforeChange();
-
 		//编辑电路
-		crun[Manager.crun.length] = body.p2.Clone(CLONE_FOR_USE);
-		crun[Manager.crun.length].coord.x += inter.x;
-		crun[Manager.crun.length].coord.y += inter.y;
-		crun[Manager.crun.length].num = Manager.crun.length;
-		++Manager.crun.length;
-
-		//将新的电路信息保存到容器
-		PutCircuitToVector();
+		var newElement = body.p.Clone(CLONE_FOR_USE);
+		newElement.x += inter.x;
+		newElement.y += inter.y;
+		newElement.index = Manager.crun.length;
+		Manager.crun.push(newElement);
 
 		//重绘电路
-		PaintCrun(crun[Manager.crun.length-1], true);
-	}
-	else //if (body.IsOnCtrl())
-	{
+		Manager.PaintCrun(newElement, true);
+	} else { //if (body.IsOnCtrl())
 		//验证
-		if (Manager.ctrl.length >= MAX_CTRL_COUNT)
-		{
+		if (Manager.ctrl.length >= MAX_CTRL_COUNT) {
 			Manager.canvas.MessageBox("电学元件超过最大数量!", "电学元件不能添加", MB_ICONWARNING);
 			return false;
 		}
 
-		//编辑前复制电路
-		CloneCircuitBeforeChange();
-
 		//编辑部分
-		ctrl[Manager.ctrl.length] = body.p3.Clone(CLONE_FOR_USE);
-		ctrl[Manager.ctrl.length].coord.x += inter.x;
-		ctrl[Manager.ctrl.length].coord.y += inter.y;
-		ctrl[Manager.ctrl.length].num = Manager.ctrl.length;
-		++Manager.ctrl.length;
-
-		//将新的电路信息保存到容器
-		PutCircuitToVector();
+		var newElement = body.p.Clone(CLONE_FOR_USE);
+		newElement.x += inter.x;
+		newElement.y += inter.y;
+		newElement.index = Manager.ctrl.length;
+		Manager.ctrl.push(newElement);
 
 		//重绘电路
-		PaintCtrl(ctrl[Manager.ctrl.length-1], true);
+		Manager.PaintCtrl(newElement, true);
 	}
 
 	return true;
-}
+};
 
 //旋转控件
 Manager.RotateCtrl = function(body, rotateAngle) {
